2c94f23cab98ab9c9ed5a8d5c7fd522ff3ee7c50
Alexander Kölbl <alexander.koelbl@student.tgm.ac.at>
2016-02-15 20:51:57 +0100

    added test

---
+++ test.py
@@ -0,0 +1,10 @@
+__author__ = 'Alex'
+
+from unittest import TestCase
+from CSVReader import CSVReader
+
+class PythonUnittests(TestCase):
+
+     def test_reader_none(self):
+        reader = CSVReader(None)
+        self.assertRaises(TypeError, reader.read)
