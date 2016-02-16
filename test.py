3514c2415010f2dab42c65774110e47cdcdc6c10
Alexander Kölbl <alexander.koelbl@student.tgm.ac.at>
2016-02-16 09:53:45 +0100

    added tests

--- test.py
+++ test.py
@@ -8,3 +8,21 @@ class PythonUnittests(TestCase):
      def test_reader_none(self):
         reader = CSVReader(None)
         self.assertRaises(TypeError, reader.read)
+
+     def test_reader_number(self):
+        reader = CSVReader(123456)
+        self.assertRaises(Exception, reader.read)
+
+     def test_reader_invalid_file(self):
+         reader = CSVReader("asdf.csv")
+         self.assertRaises(FileNotFoundError, reader.read)
+
+     def test_success(self):
+         reader = CSVReader("testfile.csv")
+         data = reader.read()
+         self.assertTrue(len(data) == 3)
+
+     def test_delimiter(self):
+         reader = CSVReader("testfile.csv")
+         reader.read()
+         self.assertEqual(reader.delimiter(), ',')
