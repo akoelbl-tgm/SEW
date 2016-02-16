501d10a7d66c5272728e7c8fd000ecb0f0c747eb
Alexander Kölbl <alexander.koelbl@student.tgm.ac.at>
2016-02-16 11:16:15 +0100

    minor changes

--- Control.py
+++ Control.py
@@ -1,16 +1,16 @@
 import csv
 
 class Control():
-    def __init__(self):
+    def __init__(self,filename):
         self.read = None
         self.delimiter = None
+        self.csvfile = filename
 
     def fill(self):
         text = ''
         delimiter = ';'
 
-        with open('testfile.csv', newline='') as f:
+        with open(self.csvfile, newline='') as f:
             reader = csv.reader(f, delimiter=';', quotechar=' ')
-
             for row in reader:
                 text += str(row)+'\n'
--- test.py
+++ test.py
@@ -1,39 +1,35 @@
 from unittest import TestCase
 from Control import Control
+import csv
+
+def csvReader(file_obj, test = False):
+        if(test):
+            reader = csv.reader(file_obj, delimiter=';')
+            for row in reader:
+                print(', '.join(row))
+        reader = csv.reader(file_obj, delimiter=';')
+        return list(reader)
 
 class PythonUnittests(TestCase):
 
-     def test_reader_none(self):
-        c = Control()
+     def test_wrongFileError(self):
+        c = Control('asdf.csv')
         self.assertRaises(TypeError, c.read)
 
      def test_delimiter(self):
-        """
-        testet ob der richtige delimiter benutzt wird
-        """
-        c = Control()
+        c = Control('testfile.csv')
         c.delimiter = ';'
         self.assertEqual(c.delimiter, ';')
 
-     def test_non_delimiter(self):
-        """
-        testet ob ein delimiter vorhanden ist
-        """
-        c = Control()
-        self.assertIsNone(c.delimiter)
-
-     def test_wrong_delimiter(self):
-        """
-        testet ob ein falscher delimiter verwendet wird
-        """
-        c = Control()
+     def test_falseDelimiter(self):
+        c = Control('testfile.csv')
         c.delimiter = ' '
         self.assertEqual(c.delimiter, ' ')
 
-     def test_wrong_delimiter2(self):
-        """
-        testet ob ein falscher delimiter verwendet wird
-        """
-        c = Control()
-        c.delimiter = ','
-        self.assertEqual(c.delimiter, ',')
+     def test_noDelimiter(self):
+        c = Control('testfile.csv')
+        self.assertIsNone(c.delimiter)
+
+     def test_contentCSV(self):
+         with open('testfile2.csv', newline='') as csvFile:
+                self.assertEqual(csvReader(csvFile),[['1', '2','3','4']])
